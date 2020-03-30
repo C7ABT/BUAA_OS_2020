ca36ac0ac59574d1804a788f030fded927c1b633
C7ABT <41655808+C7ABT@users.noreply.github.com>
2020-03-30 11:57:24 +0800

    Create  palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+        int n;
+        scanf("%d",&n);
+        int i = 0, j = 0,k,a[10];
+        while(n > 0)
+        {
+                a[i++] = n % 10;
+                n /= 10;
+        }
+        k = i - 1;
+        while(a[j] == a[k] && j <= i - 1)
+        {
+                j++;
+                k--;
+        }
+        if(j > i - 1){
+                printf("Y");
+        }else{
+                printf("N");
+        }
+        return 0;
+}

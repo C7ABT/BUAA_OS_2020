450e170d5b9fcaae4214997ff03f006bfabdca35
C7ABT <41655808+C7ABT@users.noreply.github.com>
2020-03-30 12:09:33 +0800

    Create search.sh

---
+++ ray/sh_test2/search.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+#First you can use grep (-n) to find the number of lines of string.
+#Then you can use awk to separate the answer.
+
+grep -n $2 $1 | awk -F: '{print $1}' > $3
+
+#grep -n $2 $1 | cut -d ":" -f 1 > $3   // Another way to do so.
